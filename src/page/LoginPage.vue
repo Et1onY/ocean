eea85347ec2b45f5bc7ef6e88ad1e7452bb1213b
Et1onY <1011542415@qq.com>
2024-10-10 18:41:41 +0800

    444

--- src/page/LoginPage.vue
+++ src/page/LoginPage.vue
@@ -5,8 +5,8 @@ import axios from "axios";
 const router = useRouter();
 const giteedata = ref({
     grant_type: 'password',
-    username: '',
-    password: '',
+    username: '1011542415@qq.com',
+    password: '2004817Lcz.',
     client_id: '194ce973ee99f056972aedc365aeb1e4fdf5b2bf38ef00b52698951a1e9ea1b9',
     client_secret: '52ac8afc8cc90c1c8cd0ce5319a0a1b17a0da3641b52de9dca365143972812fa',
     scope: 'projects'
@@ -17,9 +17,9 @@ const handleClick = () => {
             if (res.data.access_token != '') {
                 router.push('/CodePage');
             } else {
-                console.log("登录出现错误");
+                alert("登录出现错误");
             }
-            console.log(res.data.access_token);
+            console.log(res.data);
         }).catch((err) => {
             console.log(err);
         })
@@ -44,11 +44,15 @@ const handleClick = () => {
                             placeholder="密码" show-password />
                     </p>
                 </div>
-                <el-checkbox label="我已阅读并同意服务协议和隐私协议" size="large" />
-                <el-button type="primary" @click="handleClick">登录</el-button>
+                <div style="display:flex;align-items: center;">
+                    <p><el-checkbox label="" size="large" /></p>
+                    <p>我已阅读并同意<span style="color: #032bec;margin: 0 5px 0 5px;">服务协议</span>和<span
+                            style="color: #032bec;margin: 0 5px 0 5px;">隐私协议</span></p>
+                </div>
+                <el-button @click="handleClick" color="#032bec">登录</el-button>
                 <p style="text-align: center">
                     <span>还未注册？</span>
-                    <span style="color: #409eff;">立即注册</span>
+                    <span style="color: #032bec;">立即注册</span>
                 </p>
             </div>
         </div>
